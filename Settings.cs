5bdd6b1f4f7b6737087dcec294840ed2d5378f15
Giovanna-Gregorio <giovanna_gregorio@univem.edu.br>
2020-03-20 14:47:10 -0300

    Adicionando o projeto

---
+++ Settings.cs
@@ -0,0 +1,8 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "cfd198970de24ffa64810d5208088c9d";
+        //"fedaf7d8863b48e197b9287d492b708e"
+    }
+}
